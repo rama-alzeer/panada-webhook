59d29f4f42e757325dc30038323a8f27c580ef42
rama-alzeer <130206984+rama-alzeer@users.noreply.github.com>
2025-11-18 10:07:50 +0100

    Update token.js

--- token.js
+++ token.js
@@ -1,14 +1,20 @@
-const { GoogleAuth } = require("google-auth-library");
+import { GoogleAuth } from 'google-auth-library';
+import fs from 'fs';
 
-async function generateAccessToken() {
-  const auth = new GoogleAuth({
-    keyFile: "./key.json", // rename your downloaded key to key.json
-    scopes: ["https://www.googleapis.com/auth/cloud-platform"],
-  });
+// Save JSON from environment variable to a temporary file
+const jsonPath = './temp-service-account.json';
+fs.writeFileSync(jsonPath, process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
 
+const auth = new GoogleAuth({
+  keyFile: jsonPath,
+  scopes: 'https://www.googleapis.com/auth/cloud-platform',
+});
+
+async function getAccessToken() {
   const client = await auth.getClient();
   const token = await client.getAccessToken();
-  console.log("ACCESS TOKEN:\n", token);
+  console.log('Access Token:', token.token); // this is your token
 }
 
-generateAccessToken();
+getAccessToken();
+
