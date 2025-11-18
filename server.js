5194f989de049d213bfd511c37c02a61cff655d6
rama-alzeer <130206984+rama-alzeer@users.noreply.github.com>
2025-11-18 01:33:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.use(express.json());]
+app.use(express.json());
 app.use(express.static(__dirname));
 
 app.post("/webhook", (req, res) => {
